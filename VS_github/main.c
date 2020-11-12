acd6b7cb022842ff997974ad0013d721d63b81ca
현준 조 <rhlehfndvkd7557@daum.net>
2020-11-12 22:53:38 +0900

    _kbhit()사용하여 컴에 부담을 줄임 (저는 확실히 팬돌아가는 소리가 작아졌는데 다른 분은 어떤지 모르겠네요)

--- VS_github/main.c
+++ VS_github/main.c
@@ -29,75 +29,78 @@ int main(void) {
 	int num = 1;
 	bool act_print = false;
 	char a_input[100];
+
 	ST_ImagePrint ST_images = { 0,0,false };
 	ST_ImagePrint* ptr_images = &ST_images;
+
 	int x = 42, y = 1;
 	SetConsoleSize(SIZE_MAXRL, SIZE_MAXUD);
 	printallline();
-
+	SetCursorVisible(true);
 
 	while (1) {
 
-		SetCursorVisible(true);
-		//방향키 설정
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-			x--;
-			if (x < MINRL)x = MINRL;
-		}
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-			x++;
-			if (x > MAXRL)x = MAXRL;
-		}
-		if (GetAsyncKeyState(VK_UP) & 0x8000) {
-			y--;
-			if (y < MINUD)y = MINUD;
-		}
-		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-			y++;
-			if (y > MAXUD)y = MAXUD;
-		}
-		for (int a = 0; a < 5; a++) {
-			if (ST_images.act == true) {
-				if (num == 1) {
-					printbaby(ptr_images);
+		if (_kbhit()) {
+			//방향키 설정
+			if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+				x--;
+				if (x < MINRL)x = MINRL;
+			}
+			if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+				x++;
+				if (x > MAXRL)x = MAXRL;
+			}
+			if (GetAsyncKeyState(VK_UP) & 0x8000) {
+				y--;
+				if (y < MINUD)y = MINUD;
+			}
+			if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+				y++;
+				if (y > MAXUD)y = MAXUD;
+			}
+			for (int a = 0; a < 5; a++) {
+				if (ST_images.act == true) {
+					if (num == 1) {
+						printbaby(ptr_images);
+					}
+					if (num == 2) {
+						printrabbit(ptr_images);
+					}
+					ST_images.act = false;
 				}
-				if (num == 2) {
-					printrabbit(ptr_images);
+			}
+
+			//shift로 이미지 선택
+			if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
+				num++;
+				Sleep(50);
+				if (num == MAX) {
+					num = 1;
 				}
-				ST_images.act = false;
 			}
-		}
 
-		//shift로 이미지 선택
-		if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
-			num++;
-			Sleep(50);
-			if (num == MAX) {
-				num = 1;
+			//스페이스로 이미지 출력 명령
+			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+				ST_images.x = x;
+				ST_images.y = y;
+				ST_images.act = true;
+				Sleep(50);
 			}
-		}
-
-		//스페이스로 이미지 출력 명령
-		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
-			ST_images.x = x;
-			ST_images.y = y;
-			ST_images.act = true;
-			Sleep(50);
-		}
-		if (GetAsyncKeyState(VK_CONTROL) & 0x8000) { //컨트롤로 텍스트 입력 받음
-			if (act_print == true) {
-				act_print = false;
+			if (GetAsyncKeyState(VK_CONTROL) & 0x8000) { //컨트롤로 텍스트 입력 받음
+				if (act_print == true) {
+					act_print = false;
+				}
+				else {
+					act_print = true;
+				}
 			}
-			else {
-				act_print = true;
-			}
+			GotoXY(x, y);
 		}
 		while (act_print) {
+			fflush(stdout);
 			fgets(a_input, sizeof(a_input), stdin);
-
 			for (int a = 0; a < strlen(a_input); a++) {
 				if (a_input[a] == '\n') {
-					
 					GotoXY(x, y);
 					act_print = false;
 				}
@@ -109,7 +112,7 @@ int main(void) {
 		}
 
 		view_cusor_position(x, y, false);
-		GotoXY(x, y);
+		
 		Sleep(50);
 
 	}
